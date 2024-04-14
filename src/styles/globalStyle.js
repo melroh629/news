83f347ade9af25cf61e671c72d429ca4776e16a4
melroh629 <jw.roh629@gmail.com>
2024-04-14 15:09:31 +0900

    chore: styled, antd, 뉴스 로고 이미지 data추가

---
+++ src/styles/globalStyle.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+    line-height: 1.5;
+    background: white;
+    color: black;
+  }
+  /* 추가적인 전역 스타일 설정이 가능합니다 */
+`;
